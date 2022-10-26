7fc26cb52fab6216d8494b79de536a1b01c7acc0
eskokado <eskokado@gmail.com>
2022-10-26 09:10:25 -0300

    feat: create card with dom

--- script.js
+++ script.js
@@ -1,8 +1,30 @@
 const BaseUrl = 'https://rickandmortyapi.com/api';
+const divObservadora = document.querySelector('.observador');
 
 async function dataAPI() {
-  const response = await fetch(`${BaseUrl}/character`);
-  console.log(response);
+  let response = []
+  try {
+    const request = await fetch(`${BaseUrl}/character`);
+    if (request.ok) {
+      response = await request.json()
+    } else {
+      console.log("request", request)
+    }
+  } catch (err) {
+    console.log("err",err)
+  }
+
+  response.results.map((elem) => {
+    divObservadora.insertAdjacentHTML(
+      'beforebegin',
+      `
+          <div class="card">
+            <h2>${elem.name}</h2>
+            <img src="${elem.image}" alt="${elem.name}" />
+          </div>
+        `,
+    );
+  });  
 }
 
 dataAPI();
